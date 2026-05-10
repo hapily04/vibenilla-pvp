6d32ba8b6508c28a1108afdb3fc13a96fb128e60
mudkipdev <mudkip@mudkip.dev>
2026-05-10 00:12:05 -0600

    fix trident hit rebound

--- src/main/java/io/github/togar2/pvp/entity/projectile/ThrownTrident.java
+++ src/main/java/io/github/togar2/pvp/entity/projectile/ThrownTrident.java
@@ -102,7 +102,7 @@ public class ThrownTrident extends AbstractArrow {
 
         this.damageDone = true;
 
-        this.setVelocity(this.velocity.mul(-0.01, -0.1, -0.01));
+        this.setVelocity(this.velocity.mul(-0.02, -0.2, -0.02));
         this.getViewersAsAudience().playSound(Sound.sound(
 				SoundEvent.ITEM_TRIDENT_HIT, Sound.Source.NEUTRAL,
 				1.0f, 1.0f
