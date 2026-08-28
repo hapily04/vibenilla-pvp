bd9ecb2016c634ee0e60e559eb9597adcfefd7cd
mudkipdev <mudkip@mudkip.dev>
2026-08-27 23:41:18 -0600

    make demo server flat world

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -5,41 +5,23 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.arguments.ArgumentEnum;
 import net.minestom.server.command.builder.arguments.ArgumentType;
-import net.minestom.server.color.Color;
-import net.minestom.server.component.DataComponents;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.EquipmentSlot;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
-import net.minestom.server.instance.anvil.AnvilLoader;
-import net.minestom.server.item.ItemStack;
-import net.minestom.server.item.Material;
-import net.minestom.server.item.component.FireworkExplosion;
-import net.minestom.server.item.component.FireworkList;
-
-import java.util.List;
+import net.minestom.server.instance.block.Block;
 
 void main() {
     var server = MinecraftServer.init();
     var instance = MinecraftServer.getInstanceManager().createInstanceContainer();
-    instance.setChunkLoader(new AnvilLoader("world"));
+    instance.setGenerator(unit -> unit.modifier().fillHeight(-64, 0, Block.STONE));
     instance.setExplosionSupplier(CombatFeatures.modernVanilla().get(FeatureType.EXPLOSION).getExplosionSupplier());
 
     MinecraftServer.getGlobalEventHandler()
             .addChild(MinestomPvP.events())
-            .addListener(AsyncPlayerConfigurationEvent.class, event -> {
-                var spawnPosition = new Pos(211.5D, 61.0D, 162.5D, -90.0F, 0.0F);
-                event.setSpawningInstance(instance);
-                event.getPlayer().setRespawnPoint(spawnPosition);
-            })
-            .addListener(PlayerSpawnEvent.class, event -> {
-                var player = event.getPlayer();
-                player.setGameMode(GameMode.CREATIVE);
-                player.getInventory().addItemStack(ItemStack.of(Material.CROSSBOW));
-                player.setEquipment(EquipmentSlot.OFF_HAND, createExplodingFireworkRocket());
-            });
+            .addListener(AsyncPlayerConfigurationEvent.class, event -> event.setSpawningInstance(instance))
+            .addListener(PlayerSpawnEvent.class, event -> event.getPlayer().setGameMode(GameMode.CREATIVE));
 
     MinecraftServer.getCommandManager().register(new Command("gamemode") {{
         var argument = ArgumentType.Enum("mode", GameMode.class).setFormat(ArgumentEnum.Format.LOWER_CASED);
@@ -51,18 +33,5 @@ void main() {
     }});
 
     MinestomPvP.init();
-    server.start("0.0.0.0", 25566);
-}
-
-private static ItemStack createExplodingFireworkRocket() {
-    var explosion = new FireworkExplosion(
-            FireworkExplosion.Shape.LARGE_BALL,
-            List.of(new Color(255, 60, 45)),
-            List.of(new Color(255, 220, 80)),
-            true,
-            true
-    );
-    var fireworks = new FireworkList(1, List.of(explosion));
-
-    return ItemStack.of(Material.FIREWORK_ROCKET).with(DataComponents.FIREWORKS, fireworks);
+    server.start("0.0.0.0", 25565);
 }
